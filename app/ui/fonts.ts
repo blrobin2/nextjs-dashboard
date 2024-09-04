1209199f6d4e476ff06e9b6534e03d9072edab63
Bennie Robinson <brobinson88@gmail.com>
2024-09-04 14:39:01 -0400

    Initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Montserrat } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const montserrat = Montserrat({ subsets: ["latin"], style: "normal" });
+export const montserratItalic = Montserrat({
+  subsets: ["latin"],
+  style: "italic",
+});
